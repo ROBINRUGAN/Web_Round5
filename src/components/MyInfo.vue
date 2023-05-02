b7b3a9c856b1890217f435ad136e44cd94569fda
ROBINRUGAN <1220045015@qq.com>
2023-05-02 18:45:30 +0800

    修改了header样式，解决了上传项目的表单bug，给用户个人中心打了样

--- src/components/MyInfo.vue
+++ src/components/MyInfo.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-        MyInfo
+    <div class="form">
+        <div>用户名：闲猫吃咸鱼</div>
+        <div>昵称：MEWWW</div>
+        <div>电话：18859522606</div>
+        <div>实名认证：已实名认证</div>
+        <div>身份证号：3****************3</div>
+        <div>真实姓名：**榜</div>
+        <div>余额：￥114514</div>
+        <div>帐号状态：正常</div>
+
     </div>
 </template>
 
