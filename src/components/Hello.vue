79df7190d7d709a1e37b3dd58494927bc914cfea
ROBINRUGAN <1220045015@qq.com>
2023-06-28 00:03:58 +0800

    处理了退出登录功能

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -9,19 +9,17 @@
     </div>
 
     <el-dropdown-menu slot="dropdown" class="menu">
-      <router-link to="/userCenter" style="text-decoration: none;" >
-              <el-dropdown-item icon="el-icon-user">个人中心</el-dropdown-item>
+      <router-link to="/userCenter" style="text-decoration: none">
+        <el-dropdown-item icon="el-icon-user">个人中心</el-dropdown-item>
       </router-link>
 
-      <router-link to="/login" style="text-decoration: none;">
-              <el-dropdown-item icon="el-icon-switch-button" >退出登录</el-dropdown-item>
-      </router-link>
-
+      <el-dropdown-item icon="el-icon-switch-button" @click.native="exit">退出登录</el-dropdown-item>
     </el-dropdown-menu>
   </el-dropdown>
 </template>
  
 <script>
+import axios from 'axios';
 import UserPhoto from "../components/UserPhoto";
 export default {
   components: {
@@ -30,8 +28,15 @@ export default {
   data() {
     return {};
   },
-  methods:{
-  }
+  methods: {
+    exit()
+    {
+      alert("拜拜~");
+      window.localStorage.removeItem('token');
+      window.localStorage.removeItem('userInfo');
+      this.$router.push("/login");
+    },
+  },
 };
 </script>
 
@@ -50,7 +55,7 @@ export default {
   width: 10rem;
 }
 li {
-    margin-left: 0.1rem;
+  margin-left: 0.1rem;
   height: 2.5rem;
   font-size: 1.1rem;
 }
