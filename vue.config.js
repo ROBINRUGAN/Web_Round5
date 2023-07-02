0e17a276739ee9f9c679bf088dd4fc60d845562c
ROBINRUGAN <1220045015@qq.com>
2023-07-03 00:54:18 +0800

    准备部署

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,15 @@ const {
   defineConfig
 } = require('@vue/cli-service')
 module.exports = {
+  assetsDir: 'static',
+  parallel: false,
+  publicPath: './',
   transpileDependencies: true,
   lintOnSave: false,
   devServer: {
     open: false, // 编译完成是否打开网页
     host: '0.0.0.0', // 指定使用地址，默认localhost,0.0.0.0代表可以被外界访问
-    port: 8080, // 访问端口
+    port: 5003, // 访问端口
     https: false, // 编译失败时刷新页面
     hot: true, // 开启热加载
     proxy: {
